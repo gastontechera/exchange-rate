6d5b9f3302c0306bed1896bc11dc384643314df0
gastontechera <123432133+gastontechera@users.noreply.github.com>
2023-09-11 12:07:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ function obtenerDatos() {
         .then(data => {
             datosCompletos = data.rates;
             mostrarDatos(datosCompletos);
+
+            // Rellenar el datalist con las monedas
+            const dataList = document.getElementById('monedasList');
+            let optionsHtml = '';
+            for (const moneda in datosCompletos) {
+                optionsHtml += `<option value="${moneda}">`;
+            }
+            dataList.innerHTML = optionsHtml;
+
         })
         .catch(error => console.error('Error:', error));
 }
@@ -15,15 +24,13 @@ function obtenerDatos() {
 function mostrarDatos(datos) {
     const resultado = document.getElementById('resultado');
     let html = '';
-    
     for (const moneda in datos) {
         html += `
-        <div class="row border-bottom py-2">
-            <div class="col-md-6 text-center">${moneda}</div>
-            <div class="col-md-6 text-center">${datos[moneda]}</div>
-        </div>`;
+            <div class="row border-bottom py-2">
+                <div class="col-md-6 text-center">${moneda}</div>
+                <div class="col-md-6 text-center">${datos[moneda]}</div>
+            </div>`;
     }
-
     resultado.innerHTML = html;
 }
 
@@ -33,7 +40,6 @@ function filtrarMoneda() {
     const datosFiltrados = Object.fromEntries(
         Object.entries(datosCompletos).filter(([moneda]) => moneda.toUpperCase().includes(term))
     );
-    
     mostrarDatos(datosFiltrados);
 }
 
@@ -41,7 +47,22 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('container', 'mt-5');
     obtenerDatos();
 
+    // Asociar datalist con input al empezar a escribir
+    document.getElementById('buscadorMoneda').addEventListener('input', function() {
+        if (this.value.length > 0) {
+            this.setAttribute('list', 'monedasList');
+        } else {
+            this.removeAttribute('list');
+        }
+    });
+
+    // Evento para buscar con la tecla Enter
     document.getElementById('buscadorMoneda').addEventListener('keyup', event => {
         if (event.key === "Enter") filtrarMoneda();
+    });
+
+    // Evento para filtrar al seleccionar una opción del autocompletado
+    document.getElementById('buscadorMoneda').addEventListener('change', function() {
+        filtrarMoneda();
     });
 });
